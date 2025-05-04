2b3e4c1a6125604fce2c4c925cad18c1387309a2
Salma-M-H <salmahassan@gmail.com>
2025-05-04 20:07:11 +0300

    Add: AdultController and ToDoController

---
+++ BackendProject-ASP.NET/OnlineSchoolForKids.Solution/OnlineSchoolForKids.API/DTOs/ToDoDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineSchoolForKids.API.DTOs
+{
+    public class ToDoDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public DateTime CreationDate { get; set; }
+        public bool Status { get; set; }
+        public int UserId { get; set; }
+    }
+}
